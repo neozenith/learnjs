f80ec0505b08c4d7bed0c7fe786069901ad324e4
Josh Peak <neozenith.dev@gmail.com>
2020-02-01 10:15:45 +1100

    build out router

---
+++ public/app.js
@@ -0,0 +1,6 @@
+'use strict';
+let learnjs = {};
+learnjs.showView = function(hash){
+  let problemView = $('<div class="problem-view">').text('Coming Soon!');
+  $('.view-container').empty().append(problemView);
+};
